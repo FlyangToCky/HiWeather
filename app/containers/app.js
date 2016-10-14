df3e97125dbd89ef9b4abc5e2adef91fe338d96a
FlyangToCky <fangyang620@163.com>
2016-10-14 16:24:50 +0800

    项目初始化(2)

---
+++ app/containers/app.js
@@ -0,0 +1,8 @@
+import {Dimensions} from 'react-native';
+let Util ={
+    size:{
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height
+    }
+}
+export default Util;
